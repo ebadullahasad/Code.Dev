ced4457e331829516eef1fa676b60e4b49d0966e
ebadullahasad <ebadullahasad456@gmail.com>
2025-04-29 14:56:43 +0500

    Component/CheckBox.jsx

--- src/Components/CheckBox.jsx
+++ src/Components/CheckBox.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { IMAGES } from "../Utils/Images";
-
+import { IMAGES } from "../Utils/images";
 
 const CheckBox = () => {
   const [isChecked, setIsChecked] = useState(true);
@@ -20,12 +19,11 @@ const CheckBox = () => {
           lg:w-[20px] md:w-[24px] sm:w-[20px] w-[18px] lg:h-[24px] md:h-[16px] 
           sm:h-[14px] h-[18px]`}
         >
-            <img
-              className="w-[20px] h-[20px]"
-              src={isChecked ? IMAGES.BOX : IMAGES.TICK1}
-              alt={IMAGES.TICKMARK}
-            />
-      
+          <img
+            className="w-[20px] h-[20px]"
+            src={isChecked ? IMAGES.BOX : IMAGES.TICK1}
+            alt={IMAGES.TICKMARK}
+          />
         </div>
       </div>
     </>
